3e750468c194b221323c69c82c86426d82f1dcb8
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-12-25 16:03:27 +0100

    update prototypes

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 11:55:41 by yez-zain          #+#    #+#             */
-/*   Updated: 2019/12/25 15:21:13 by yez-zain         ###   ########.fr       */
+/*   Updated: 2019/12/25 16:02:29 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,29 @@ typedef	struct		s_config
 	double			x;
 	double			y;
 	t_complex		c;
-	int				(*fractal_iterations)(t_complex z0, t_complex c);
+	int				iterations;
+	int				setup;
+	int				(*fractal)(t_complex z0, t_complex c, int iterations_max);
 }					t_config;
 
 t_complex			add(t_complex a, t_complex b);
 
 t_complex			multiply(t_complex a, t_complex b);
 
+double				module(t_complex z);
+
 int					ft_strlen(const char *s);
 
 int					ft_strcmp(const char *s1, const char *s2);
+
+int					julia(t_complex z0, t_complex c, int iterations_max);
+
+int					mandelbrot(t_complex z0, t_complex c, int iterations_max);
+
+int					button_operations(int button, int x, int y, void *param);
+
+int					mouse_mouve_operations(int x, int y, void *param);
+
+int					window_operations(int key, void *param);
 
 #endif
