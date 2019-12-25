a04b3f0833e7b180da052e40853434d408cfa977
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-12-25 19:07:22 +0100

    update tools

--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 14:46:27 by yez-zain          #+#    #+#             */
-/*   Updated: 2019/12/25 16:20:03 by yez-zain         ###   ########.fr       */
+/*   Updated: 2019/12/25 18:47:25 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,39 +46,30 @@ int			ft_strcmp(const char *s1, const char *s2)
 	return (0);
 }
 
-int			julia(int x, int y, t_config *config)
+void		*ft_memset(void *b, int c, size_t len)
 {
-	int			iterations;
-	t_complex	z;
+	size_t	i;
 
-	iterations = 1;
-	z.re = x / config->zoom + config->x;
-	z.im = y / config->zoom + config->y;
-	while (iterations < config->iterations && module(z) < 2.0)
+	i = 0;
+	while (i < len)
 	{
-		z = add(multiply(z, z), config->c);
-		iterations++;
+		((unsigned char*)b)[i] = (unsigned char)c;
+		i = i + 1;
 	}
-	return (iterations);
+	return (b);
 }
 
-int			mandelbrot(int x, int y, t_config *config)
+void		*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int			iterations;
-	t_complex	z;
-	t_complex	c;
+	size_t	i;
 
-	iterations = 1;
-	z.re = 0.0;
-	z.im = 0.0;
-	c.re = x / config->zoom + config->x;
-	c.im = y / config->zoom + config->y;
-	while (iterations < config->iterations && module(z) < 2.0)
+	i = 0;
+	while (i < n)
 	{
-		z = add(multiply(z, z), c);
-		iterations++;
+		((char*)dst)[i] = ((char*)src)[i];
+		i = i + 1;
 	}
-	return (iterations);
+	return (dst);
 }
 
 void		draw(t_config *config)
@@ -86,8 +77,10 @@ void		draw(t_config *config)
 	int		i;
 	int		j;
 	int		iter;
+	t_image	*image;
 
 	i = 0;
+	image = &(config->image);
 	while (i < WINDOW_WIDTH)
 	{
 		j = 0;
@@ -95,11 +88,15 @@ void		draw(t_config *config)
 		{
 			iter = config->fractal(i, j, config);
 			if (iter == config->iterations)
-				mlx_pixel_put(config->mlx_ptr, config->mlx_win, i, j, 0x0);
+				ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
 			else
-				mlx_pixel_put(config->mlx_ptr, config->mlx_win, i, j, iter * 500);
+				ft_memcpy(image->data + (j * image->size_line + i * 4),
+					config->color + (iter % 50), 4);
 			j++;
 		}
 		i++;
 	}
+	mlx_clear_window(config->mlx_ptr, config->mlx_win);
+	mlx_put_image_to_window(config->mlx_ptr, config->mlx_win,
+		image->mlx_img, 0, 0);
 }
