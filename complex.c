1d8913dd90933ec3fb42eac2513ead6443d83fb8
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-12-25 16:03:43 +0100

    add module function

--- complex.c
+++ complex.c
@@ -6,7 +6,7 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 14:43:07 by yez-zain          #+#    #+#             */
-/*   Updated: 2019/12/25 14:45:45 by yez-zain         ###   ########.fr       */
+/*   Updated: 2019/12/25 15:39:19 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,9 @@ t_complex		multiply(t_complex a, t_complex b)
 	c.re = (a.re * b.re - a.im * b.im);
 	c.im = (a.re * b.im + a.im * b.re);
 	return (c);
+}
+
+double			module(t_complex z)
+{
+	return (sqrt(z.re * z.re + z.im * z.im));
 }
