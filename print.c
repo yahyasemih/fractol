b6af276f0f7862514dc2cb3c79632c79258c8290
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-12-26 15:18:26 +0100

    fix alpha channel problem while priting image

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 19:18:27 by yez-zain          #+#    #+#             */
-/*   Updated: 2019/12/25 20:23:33 by yez-zain         ###   ########.fr       */
+/*   Updated: 2019/12/26 15:11:31 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ void		print_scene(t_config *config)
 	write(fd, &height, 4);
 	write(fd, "\x0\x0\x0\x0\x0\x0\x0\x0", 8);
 	write(fd, config->image.data, size);
+	write(1, config->image.data, size);
 	close(fd);
 }
--- threads.c
+++ threads.c
@@ -6,11 +6,28 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/26 12:20:13 by yez-zain          #+#    #+#             */
-/*   Updated: 2019/12/26 12:30:19 by yez-zain         ###   ########.fr       */
+/*   Updated: 2019/12/26 15:16:31 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void		set_pixel(int i, int j, int iter, t_config *config)
+{
+	t_image		*image;
+
+	image = &(config->image);
+	if (iter == config->iterations)
+	{
+		ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
+	}
+	else
+	{
+		ft_memcpy(image->data + (j * image->size_line + i * 4),
+			config->color + (iter % 50), 3);
+		ft_memset(image->data + (j * image->size_line + i * 4 + 3), 0, 1);
+	}
+}
 
 void			*left_up_thread(void *argument)
 {
@@ -18,22 +35,16 @@ void			*left_up_thread(void *argument)
 	int			j;
 	int			iter;
 	t_config	*config;
-	t_image		*image;
 
 	i = 0;
 	config = (t_config*)argument;
-	image = &(config->image);
 	while (i < WINDOW_WIDTH / 2)
 	{
 		j = 0;
 		while (j < WINDOW_HEIGHT / 2)
 		{
 			iter = config->fractal(i, j, config);
-			if (iter == config->iterations)
-				ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
-			else
-				ft_memcpy(image->data + (j * image->size_line + i * 4),
-					config->color + (iter % 50), 4);
+			set_pixel(i, j, iter, config);
 			j++;
 		}
 		i++;
@@ -47,22 +58,16 @@ void			*right_up_thread(void *argument)
 	int			j;
 	int			iter;
 	t_config	*config;
-	t_image		*image;
 
 	i = WINDOW_WIDTH / 2;
 	config = (t_config*)argument;
-	image = &(config->image);
 	while (i < WINDOW_WIDTH)
 	{
 		j = 0;
 		while (j < WINDOW_HEIGHT / 2)
 		{
 			iter = config->fractal(i, j, config);
-			if (iter == config->iterations)
-				ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
-			else
-				ft_memcpy(image->data + (j * image->size_line + i * 4),
-					config->color + (iter % 50), 4);
+			set_pixel(i, j, iter, config);
 			j++;
 		}
 		i++;
@@ -76,22 +81,16 @@ void			*left_down_thread(void *argument)
 	int			j;
 	int			iter;
 	t_config	*config;
-	t_image		*image;
 
 	i = 0;
 	config = (t_config*)argument;
-	image = &(config->image);
 	while (i < WINDOW_WIDTH / 2)
 	{
 		j = WINDOW_HEIGHT / 2;
 		while (j < WINDOW_HEIGHT)
 		{
 			iter = config->fractal(i, j, config);
-			if (iter == config->iterations)
-				ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
-			else
-				ft_memcpy(image->data + (j * image->size_line + i * 4),
-					config->color + (iter % 50), 4);
+			set_pixel(i, j, iter, config);
 			j++;
 		}
 		i++;
@@ -105,22 +104,16 @@ void			*right_down_thread(void *argument)
 	int			j;
 	int			iter;
 	t_config	*config;
-	t_image		*image;
 
 	i = WINDOW_WIDTH / 2;
 	config = (t_config*)argument;
-	image = &(config->image);
 	while (i < WINDOW_WIDTH)
 	{
 		j = WINDOW_HEIGHT / 2;
 		while (j < WINDOW_HEIGHT)
 		{
 			iter = config->fractal(i, j, config);
-			if (iter == config->iterations)
-				ft_memset(image->data + (j * image->size_line + i * 4), 0, 4);
-			else
-				ft_memcpy(image->data + (j * image->size_line + i * 4),
-					config->color + (iter % 50), 4);
+			set_pixel(i, j, iter, config);
 			j++;
 		}
 		i++;
